d1e4caf1e14c432a6eb8b571a58cc8efc0ea4527
Enrique Trigoso <enrique.el.capo@gmail.com>
2025-01-30 07:41:30 -0500

    style: Improving styles of the project page

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -112,26 +112,23 @@ const Project = () => {
 
                             return (
                               (
-                                <div
-                                  key={index}
-                                  className='group relative overflow-hidden px-3 py-2 md:px-4 md:py-2.5 bg-gradient-to-r from-gray-600/10 to-gray-600/10 rounded-3xl border border-gray-500/10 hover:border-gray-200/30 transition-all duration-300 cursor-default'
-                                  data-aos='fade-right' data-aos-duration='1000' data-aos-delay={delay}
-                                >
+                                <div key={index} data-aos='fade-right' data-aos-duration='1000' data-aos-delay={delay}>
+                                  <div
+                                    className='group relative overflow-hidden px-3 md:px-4 py-2 md:py-2.5 bg-gradient-to-r from-gray-600/10 to-gray-600/10 rounded-3xl border border-gray-500/10 hover:border-gray-200/30 transition-all duration-1000 cursor-default'
+                                  >
+                                    <div className='w-full absolute inset-0 bg-gradient-to-r from-gray-800/0 to-gray-100/0 group-hover:from-gray-900/10 group-hover:to-gray-400/10 transition-all duration-1000'>
+                                    </div>
 
-                                  <div className='absolute inset-0 bg-gradient-to-r from-gray-800/0 to-gray-100/0 group-hover:from-gray-900/10 group-hover:to-gray-400/10 transition-all duration-500'>
+                                    <div className='relative flex items-center gap-1.5 md:gap-2'>
+                                      <Package />
+                                      <span>{technology.name}</span>
+                                    </div>
+
                                   </div>
-
-                                  <div className='relative flex items-center gap-1.5 md:gap-2'>
-                                    <Package />
-                                    <span>{technology.name}</span>
-                                  </div>
-
                                 </div>
                               )
                             )
-
                           })
-
                         }
 
                       </div>
@@ -139,8 +136,7 @@ const Project = () => {
                     </div>
                   </div>
 
-
-                  <div>
+                  <div className='overflow-hidden'>
                     <div
                       className='relative rounded-2xl overflow-hidden bg-black border border-white/10 shadow-2xl group'
                       data-aos="fade-left" data-aos-duration='1000' data-aos-delay="200"
@@ -154,7 +150,10 @@ const Project = () => {
                       />
                     </div>
 
-                    <div className='mt-6 bg-white/[0.02] backdrop-blur-xl rounded-2xl p-8 border border-white/10 space-y-4 hover:border-white/20 transition-colors duration-300 group md:space-y-4'>
+                    <div
+                      className='mt-6 bg-white/[0.02] backdrop-blur-xl rounded-2xl p-8 border border-white/10 space-y-4 hover:border-white/20 transition-colors duration-300 group md:space-y-4'
+                      data-aos="fade-left" data-aos-duration='1000' data-aos-delay="200"
+                    >
                       <BorderTrail
                         style={{
                           boxShadow:
@@ -174,19 +173,19 @@ const Project = () => {
                             const delay = (index * 150) + 250
 
                             return (
-                              <li
-                                key={index}
-                                className='group flex items-start space-x-3 p-2.5 md:p-3.5 rounded-xl hover:bg-white/5 transition-all duration-300 border border-transparent hover:border-white/10'
-                                data-aos='fade-left' data-aos-duration='1000' data-aos-delay={delay}
-                              >
-                                <div className='relative mt-2'>
-                                  <div className='absolute -inset-1 bg-gradient-to-r from-gray-950/20 to-gray-600/20 rounded-full blur group-hover:opacity-100 opacity-0 transition-opacity duration-300'></div>
-                                  <div className="relative w-1.5 h-1.5 md:w-2 md:h-2 rounded-full bg-gradient-to-r from-gray-800 to-gray-500 group-hover:scale-125 transition-transform duration-300"></div>
-                                </div>
-                                <span>
-                                  {feature}
-                                </span>
-                              </li>
+                              <div key={index} data-aos='fade-left' data-aos-duration='1000' data-aos-delay={delay} data-aos-offset='-100'>
+                                <li
+                                  className='group flex items-start space-x-3 p-2.5 md:p-3.5 rounded-xl hover:bg-white/5 transition-all duration-300 border border-transparent hover:border-white/10 animate-fadeIn'
+                                >
+                                  <div className='relative mt-2'>
+                                    <div className='absolute -inset-1 bg-gradient-to-r from-gray-950/20 to-gray-600/20 rounded-full blur group-hover:opacity-100 opacity-0 transition-opacity duration-300'></div>
+                                    <div className="relative w-1.5 h-1.5 md:w-2 md:h-2 rounded-full bg-gradient-to-r from-gray-800 to-gray-500 group-hover:scale-125 transition-transform duration-300"></div>
+                                  </div>
+                                  <span>
+                                    {feature}
+                                  </span>
+                                </li>
+                              </div>
                             )
                           })
                         }
